fbad402beb5eec32f5e556ff00bc30cccd238d7e
Dinesh <dineshsaravanank@gmail.com>
2023-01-08 23:35:40 +0800

    Read config and establish connection to the database
    Read the app.env file and establish connection to the database
    when the app starts

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"poker/internal/config"
+
+	_ "github.com/lib/pq"
+	db "poker/database/sqlc"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	c, err := config.LoadConfig(".")
+
+	if err != nil {
+		log.Fatalf("cannot load config: %v", err)
+	}
+
+	conn, err := sql.Open(c.DBDriver, c.DBSource)
+	if err != nil {
+		log.Fatalf("cannot connect to db: %v", err)
+	}
+
+	store := db.New(conn)
+	log.Printf("store: %v", store)
 }
